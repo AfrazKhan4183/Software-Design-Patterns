1acac31beca4e141925a4825879945db5cf6fcf3
Afraz Khan <afrazmaxxx4183@gmail.com>
2017-11-20 16:03:28 +0500

    Singleton-Pattern

---
+++ Singleton_Design_Pattern/Singleton_Design_Pattern/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton_Design_Pattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            singleton f = singleton.make_instance();
+            Console.Write(f.getname());
+
+        }
+    }
+}
---
+++ Singleton_Design_Pattern/Singleton_Design_Pattern/singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Singleton_Design_Pattern
+{
+    class singleton
+       
+    { private static singleton instance = null;
+        string name;
+        public string getname()
+        {
+            return name;
+        }
+        private singleton()
+        {
+
+        }
+        public static singleton make_instance()
+        {
+            if(instance==null)
+            {
+                instance = new singleton();
+                Console.WriteLine("newly made");
+                instance.name = "khan";
+            }
+
+          
+            return instance;
+        
+        }
+
+
+    }
+}
